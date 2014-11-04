cd7aa09e29a97e20cce009df5da459eb58ecfad4
whispyy <Whispy@10.19.0.251>
2014-11-04 16:37:57 +0100

    add code to city

--- src/City.java
+++ src/City.java
@@ -1,4 +1,15 @@
 
 public class City {
 
+	public void distributeLetters(){
+		list<letter<?>> bag = new Arraylist<letter<?>>(postbox):
+		for(letter<?> letter: bag){
+			letter.getReceiver().receiveletter(letter);
+			postbox.remove(letter);
+		}
+	}
+
+	public void sendLetter(letter<?>letter){
+		postBox.add(letter);
+	}
 }
