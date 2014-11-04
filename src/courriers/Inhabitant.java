4ae7d93d2141ca31752be5bcb706b6cf4fbaa737
whispyy <Whispy@10.19.0.251>
2014-11-04 16:39:29 +0100

    add class inhabitant with some code

---
+++ src/courriers/Inhabitant.java
@@ -0,0 +1,12 @@
+package courriers;
+
+public class Inhabitant {
+	public void sendletter(letter<?>letter){
+		account.debit(letter.getCost());
+		getCity().sendletter(letter);
+	}
+
+	public void receiveLetter(letter<?>letter){
+		letter.doAction();
+	}
+}
