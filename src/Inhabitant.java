e304d38bbc677bb2228967b012f7ec289fdfc715
Grimnack <matthieu.caron@laposte.net>
2014-11-13 15:23:37 +0100

    Ajout de classe BankAccount

--- src/Inhabitant.java
+++ src/Inhabitant.java
@@ -1,6 +1,18 @@
 
 
 public class Inhabitant {
+	
+	//Attributes
+	protected String name ; 
+	protected City city;
+	protected BankAccount bankaccount ;
+	
+	public Inhabitant(City c,String n,BankAccount b){
+		this.name=n;
+		this.city = c;
+		this.bankaccount = b;
+	}
+	
 	public void sendletter(letter<?>letter){
 		account.debit(letter.getCost());
 		getCity().sendletter(letter);
